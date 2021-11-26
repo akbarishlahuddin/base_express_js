c0c3f6d203abea68d1a66786b09881559bb04dee
akbarishlahuddin <94848337+akbarishlahuddin@users.noreply.github.com>
2021-11-26 21:45:49 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '2133290397:AAHIgB1ZusJ-m2-TluhcnWMfZQNwfRPMk1U';
+const token = '2038676423:AAFpSeeFkFSP2w9OjwAqn5Fyldcxq4tNO4g';
 const bot = new TelegramBot(token, {polling: true});
 
 
